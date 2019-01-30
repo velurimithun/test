50d9f902469a989c060f97e21404072534ed7a9b
Veluri Mithun <velurimithun38@gmail.com>
2019-01-30 23:00:00 +0530

    Add system permissions
    Signed-off-by: Veluri Mithun <velurimithun38@gmail.com>

--- app/src/main/java/com/example/test/controlBot.java
+++ app/src/main/java/com/example/test/controlBot.java
@@ -4,12 +4,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import org.webrtc.PeerConnectionFactory;
+
 public class controlBot extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_control_bot);
+
+        //PeerConnectionFactory.
     }
 
     public void moveForwad(View view) {
